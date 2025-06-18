bbbb8bdcb5b8ec3a6e3355522ea6278748747bbb
Yaloys <yanakyrpushko@gmail.com>
2025-06-18 15:42:07 +0300

    implemented pagination, filtering and sorting of Product list

---
+++ JewelryStore.BLL/DTOs/Product/ProductSearchRequest.cs
@@ -0,0 +1,16 @@
+﻿using JewelryStore.DAL.Pagination;
+namespace JewelryStore.BLL.DTOs.Product
+{
+	public class ProductSearchRequest :PagedRequest
+	{
+        public string? SearchName { get; set; }
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+        public string? Metal { get; set; }
+        public string? Stone { get; set; }
+        public string? Manufacturer { get; set; }
+        public string? SortBy { get; set; } = "price"; // name, price, weight
+        public bool SortDescending { get; set; } = false;
+    }
+}
+
---
+++ JewelryStore.DAL/Pagination/PagedRequest.cs
@@ -0,0 +1,17 @@
+﻿namespace JewelryStore.DAL.Pagination
+{
+	public class PagedRequest
+	{
+        private const int MaxPageSize = 30;
+        private int _pageSize = 10;
+
+        public int PageNumber { get; set; } = 1;
+
+        public int PageSize
+        {
+            get { return _pageSize; }
+            set { _pageSize = value > MaxPageSize ? MaxPageSize : value; }
+        }
+    }
+}
+
